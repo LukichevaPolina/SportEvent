f838f63c050fb4dcd2e6fcab08b4b1b68024421e
Alyona Uskova <aluskova01@yandex.ru>
2022-04-19 23:02:22 +0300

    Start frontend commit@

---
+++ frontend/app/src/main/java/com/sport/event/Onbording3.kt
@@ -0,0 +1,22 @@
+package com.sport.event
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Onbording3 : AppCompatActivity() {
+
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onbording3)
+        button = findViewById(R.id.btnNext)
+        button.setOnClickListener() {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+            //finish()
+        }
+    }
+}
